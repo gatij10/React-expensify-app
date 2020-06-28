afe6fa3d0107c0fde6568a07b448a140210f7f2a
gatij10 <Your-Email>
2020-06-28 23:01:25 +0530

    Setup dev dependencied and dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,15 @@
 //entry -> output
 const path = require("path");
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = (env) => {
   const isProduction = env === "production";
-  const CSSExtract = new ExtractTextPlugin('styles.css')
+  const CSSExtract = new ExtractTextPlugin("styles.css");
 
   return {
     entry: "./src/app.js",
     output: {
-      path: path.join(__dirname, "public"),
+      path: path.join(__dirname, "public", "dist"),
       filename: "bundle.js",
     },
     //loader
@@ -23,30 +23,30 @@ module.exports = (env) => {
         {
           test: /\.s?css$/,
           use: CSSExtract.extract({
-              use: [
-                  {
-                      loader: 'css-loader',
-                      options: {
-                          sourceMap: true
-                      }
-                  },
-                  {
-                      loader: 'sass-loader',
-                      options: {
-                          sourceMap: true
-                      }
-                  }
-              ]
-          })
-        }]
+            use: [
+              {
+                loader: "css-loader",
+                options: {
+                  sourceMap: true,
+                },
+              },
+              {
+                loader: "sass-loader",
+                options: {
+                  sourceMap: true,
+                },
+              },
+            ],
+          }),
+        },
+      ],
     },
-    plugins: [
-        CSSExtract
-    ],
-    devtool: isProduction ? 'source-map' : "inline-source-map",
+    plugins: [CSSExtract],
+    devtool: isProduction ? "source-map" : "inline-source-map",
     devServer: {
       contentBase: path.join(__dirname, "public"),
       historyApiFallback: true,
+      publicPath: '/dist/'
     },
   };
 };
