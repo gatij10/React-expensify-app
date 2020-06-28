980b869064261bfaec5c03109df411d6921daa19
gatij10 <Your-Email>
2020-06-29 00:04:39 +0530

    Added expense total feature

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,13 +1,15 @@
-import React from 'react'
-import {BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom'
-import ExpenseList from './ExpenseList'
-import ExpenseListFilters from './ExpenseListFilters'
+import React from "react";
+import { BrowserRouter, Route, Switch, Link, NavLink } from "react-router-dom";
+import ExpenseList from "./ExpenseList";
+import ExpenseListFilters from "./ExpenseListFilters";
+import ExpensesSummary from "./ExpensesSummary";
 
 const ExpenseDashboardPage = () => (
-    <div>
-        <ExpenseListFilters/>
-        <ExpenseList/>
-    </div>
-)
+  <div>
+    <ExpensesSummary />
+    <ExpenseListFilters />
+    <ExpenseList />
+  </div>
+);
 
 export default ExpenseDashboardPage;
